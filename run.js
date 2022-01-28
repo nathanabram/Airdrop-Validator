947cedccc27955f9575cf242198f770ce50efe41
nathanabram <nathan@nathanabram.com>
2022-01-28 15:06:08 -0500

    cleaned up

--- run.js
+++ run.js
@@ -1,8 +1,5 @@
 const { MerkleTree } = require("./merkleTree.js");
-const fs = require("fs");
-const { keccak256, bufferToHex } = require("ethereumjs-util");
 
-// get leafs as an array
 let addresses = [
 	"0x05vLRJkqeFpN6mdZXX6s6vylj554HSdymHME4sNMI04ItHWNMA85lr1QeCjhSNXk",
 	"0xQme29Is9rEmCACOKfpeSWDkNjDQKnPwL33jQIAgA91dKFantrD2PnFxjOwDIQZrk",
@@ -21,10 +18,9 @@ const getProofFromHexLeaf = (tree, leaf) => {
 	return tree.getProof(bufferLeaf);
 };
 
-let tree = generateTreeFromHexLeafs(addresses);
+let exampleMerkleTree = generateTreeFromHexLeafs(addresses);
+
 let proof = getProofFromHexLeaf(
-	tree,
+	exampleMerkleTree,
 	"0x4ED9I35HK4r8iKKxNobkIkykFtzo8V0GYgz1OgbAWkG4Iw4BdpQXpPutk3Q4EBNI"
 );
-
-console.log(proof);
